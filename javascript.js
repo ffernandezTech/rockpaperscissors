736df56cbdde21fb4328ff734da021d51e59520e
ffrndzTech <199731008+ffernandezTech@users.noreply.github.com>
2025-03-28 02:18:01 -0700

    feat: created getHumanChoice function

--- javascript.js
+++ javascript.js
@@ -8,25 +8,48 @@ function getComputerChoice(){
     // console.log(randomNum);
 
 
+    //Made all returns capatilized so we can uppercase the user's input for comparison
     //If the number is less than or equal to 100 return Rock;
     if(randomNum<+100)
     {
-        return 'Rock';
+        return 'ROCK';
     }
     
 
     //If the number is great than 100 and less than or equal to 200 return Paper;
     else if(randomNum>100 && randomNum <= 200)
     {
-      return 'Paper';  
+      return 'PAPER';  
     }
         
     //If the number is great than 200 and less than or equal to 300 return Scissors;
     else if(randomNum>200 && randomNum <= 300)
     {
-        return 'Scissor';
+        return 'SCISSOR';
     }
 
 }
 
+
+
+function getHumanChoice(){
+
+    //Prompt user for their input
+    let userInput = prompt('Enter Rock Paper or Scissor');
+
+    //checking that the userInput has our desired value and its uppercase for later.
+    // console.log(userInput.toUpperCase());
+
+    //returning the value in caps
+    return(userInput.toUpperCase());
+
+}
+
+
+
+
+
+
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
