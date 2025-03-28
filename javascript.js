de756c9f4583de25003db0705f2e24075ac5be4a
ffrndzTech <199731008+ffernandezTech@users.noreply.github.com>
2025-03-28 02:06:23 -0700

    feat: created compute function

--- javascript.js
+++ javascript.js
@@ -1,11 +1,32 @@
 // console.log('Hello World');
 
 function getComputerChoice(){
-   let randomNum= Math.random();
 
-    console.log(randomNum);
+//Get a randomNum between 1-300. Because of 0 we add 1 so it's 1-300. Instead of 0-299(I think)
+    let randomNum= Math.floor(Math.random() *300)+1;
+    // checking that randNum is desired output
+    // console.log(randomNum);
+
+
+    //If the number is less than or equal to 100 return Rock;
+    if(randomNum<+100)
+    {
+        return 'Rock';
+    }
+    
+
+    //If the number is great than 100 and less than or equal to 200 return Paper;
+    else if(randomNum>100 && randomNum <= 200)
+    {
+      return 'Paper';  
+    }
+        
+    //If the number is great than 200 and less than or equal to 300 return Scissors;
+    else if(randomNum>200 && randomNum <= 300)
+    {
+        return 'Scissor';
+    }
+
 }
 
-
-
-getComputerChoice();
+console.log(getComputerChoice());
