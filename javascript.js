b7db347a808b3910734de26a279468cb4f0408e1
ffrndzTech <199731008+ffernandezTech@users.noreply.github.com>
2025-03-28 23:58:55 -0700

    feat: created variables and functions

--- javascript.js
+++ javascript.js
@@ -2,54 +2,86 @@
 
 function getComputerChoice(){
 
-//Get a randomNum between 1-300. Because of 0 we add 1 so it's 1-300. Instead of 0-299(I think)
-    let randomNum= Math.floor(Math.random() *300)+1;
-    // checking that randNum is desired output
-    // console.log(randomNum);
-
-
-    //Made all returns capatilized so we can uppercase the user's input for comparison
-    //If the number is less than or equal to 100 return Rock;
-    if(randomNum<+100)
-    {
-        return 'ROCK';
+    //Get a randomNum between 1-300. Because of 0 we add 1 so it's 1-300. Instead of 0-299(I think)
+        let randomNum= Math.floor(Math.random() *300)+1;
+        // checking that randNum is desired output
+        // console.log(randomNum);
+    
+    
+        //Made all returns capatilized so we can uppercase the user's input for comparison
+        //If the number is less than or equal to 100 return Rock;
+        if(randomNum<+100)
+        {
+            return 'ROCK';
+        }
+        
+    
+        //If the number is great than 100 and less than or equal to 200 return Paper;
+        else if(randomNum>100 && randomNum <= 200)
+        {
+          return 'PAPER';  
+        }
+            
+        //If the number is great than 200 and less than or equal to 300 return Scissors;
+        else if(randomNum>200 && randomNum <= 300)
+        {
+            return 'SCISSOR';
+        }
+    
     }
     
-
-    //If the number is great than 100 and less than or equal to 200 return Paper;
-    else if(randomNum>100 && randomNum <= 200)
-    {
-      return 'PAPER';  
+    
+    
+    function getHumanChoice(){
+    
+        //Prompt user for their input
+        let userInput = prompt('Enter Rock Paper or Scissor');
+    
+        //checking that the userInput has our desired value and its uppercase for later.
+        // console.log(userInput.toUpperCase());
+    
+        //returning the value in caps
+        return(userInput.toUpperCase());
+    
     }
+    // keep track of the score both each player
+    let humanScore=0, computerScore=0;
+    
+    
+    
+    
+    
+    
+    
+    // console.log(getComputerChoice());
+    // console.log(getHumanChoice());
+    
+    
+    
+    
+    function playround(humanChoice, computerChoice){
+        // if(humanChoice == computerChoice)
+        // {
+        //     console.log('test');
+        //     console.log(`${humanChoice} vs ${computerChoice} is a draw!`);
+        // }
+        // else{
+        //     console.log('test else');
+        // }
         
-    //If the number is great than 200 and less than or equal to 300 return Scissors;
-    else if(randomNum>200 && randomNum <= 300)
-    {
-        return 'SCISSOR';
+    
+        // console.log('in playround function');
+    
     }
-
-}
-
-
-
-function getHumanChoice(){
-
-    //Prompt user for their input
-    let userInput = prompt('Enter Rock Paper or Scissor');
-
-    //checking that the userInput has our desired value and its uppercase for later.
-    // console.log(userInput.toUpperCase());
-
-    //returning the value in caps
-    return(userInput.toUpperCase());
-
-}
-
-
-
-
-
-
-
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+    
+    
+    
+    const computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    
+    
+    const humanSelection = getHumanChoice();
+    console.log(humanSelection);
+    
+    
+    playround(humanSelection,computerScore);
